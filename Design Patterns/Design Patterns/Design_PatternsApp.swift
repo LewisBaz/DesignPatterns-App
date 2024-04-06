6d9947f5dd315ab7573f2c375ef0d9cd5d793bc6
Lewis <lev.bazhkov@aisa.ru>
2024-04-06 20:45:04 +0300

    init project

---
+++ Design Patterns/Design Patterns/Design_PatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_PatternsApp.swift
+//  Design Patterns
+//
+//  Created by Lewis on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Design_PatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
